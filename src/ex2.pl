4cd607cd5bbd6e995cc8ec1dfaa4d0f8ac5d3f21
Linna Wang <lw4715@ic.ac.uk>
2018-02-16 20:47:55 +0000

    ex3

--- src/ex2.pl
+++ src/ex2.pl
@@ -1,35 +1,33 @@
 :- compile('gorgias-src-0.6d/lib/gorgias.pl').
 :- compile('gorgias-src-0.6d/ext/lpwnf.pl').
+
 % ex 2 - US bank hack
-% tech
-% highLevelSkill(Att) :- hijackCorporateClouds(Att). %T1
-% highLevelSkill(Att) :- sophisticatedMalware(M), malwareUsedInAttack(M, Att). % T2
-% ex2
 rule(t1, culpritIsFrom(X, Att), [majorityIpOrigin(X, Att)]).
-rule(t2, not(culpritIsFrom(X, Att)), [spoofedIp(Att), ipOrigin(X, Att)]).
+rule(t2, not(culpritIsFrom(X, Att)), [spoofedIp(IP), ipGeoloc(G, IP), geolocInCountry(G, X), attackSourceIP(IP, Att)]).
 rule(t3, culpritIsFrom(X, Att), [firstLanguage(L, X), sysLanguage(L, Att)]).
-rule(t4, culpritIsFrom(X, Att), [infraRegisteredIn(X, Infra), infraUsed(Infra, Att)]).
+rule(t4, highLevelSkill(Att), [hijackCorporateClouds(Att)]).
+rule(t5, highLevelSkill(Att), [sophisticatedMalware(M), malwareUsedInAttack(M, Att)]).
+rule(t6(X), culpritIsFrom(X, Att), [infraRegisteredIn(X, Infra), infraUsed(Infra, Att)]).
 
-rule(o1, hasMotive(C, Att), [hasEconomicMotive(C, Industry), industry(Industry, T), target(T, Att)]).
-rule(o2, requireHighResource(Att), [highSecurity(T), target(T, Att)]).
-rule(o3, requireHighResource(Att), [largeNumberOfVictims(Att)]).
-rule(o4, requireHighResource(Att), [highVolumeAttack(Att),longDurationAttack(Att)]).
-rule(o5, governmentLinked(P,C), [geolocatedInGovFacility(P,C)]).
-rule(o6, governmentLinked(P,C), [publicCommentsRelatedToGov(P,C)]).
-
-
+rule(o1, requireHighResource(Att), [highLevelSkill(Att)]).
+rule(o2, hasCapability(_, Att), [not(requireHighResource(Att))]).
+rule(o3(C), hasCapability(C, Att), [requireHighResource(Att), hasResources(C)]).
+rule(o4(C), hasMotive(C, Att), [hasPoliticalMotive(C, T), target(T, Att)]).
+rule(o5(C,T), hasPoliticalMotive(C, T), [imposedSanctions(T, C)]).
+rule(o6(C), hasMotive(C, Att), [hasEconomicMotive(C, Industry), industry(Industry, T), target(T, Att)]).
+rule(o7, requireHighResource(Att), [highSecurity(T), target(T, Att)]).
+rule(o8, requireHighResource(Att), [highVolumeAttack(Att),longDurationAttack(Att)]).
+rule(o9(P,C), governmentLinked(P,C), [geolocatedInGovFacility(P,C)]).
+rule(o10(P,C), governmentLinked(P,C), [publicCommentsRelatedToGov(P,C)]).
 
 rule(s1, isCulprit(G,Att), [claimedResponsibility(G,Att)]).
 rule(s2, isCulprit(C,Att), [hasMotive(C,Att),hasCapability(C,Att)]).
-
 rule(s3, isCulprit(C,Att), [hasMotive(C,Att),culpritIsFrom(C,Att)]).
 rule(s4, not(isCulprit(C,Att)), [culpritIsFrom(C,Att),nothasCapability(C,Att)]).
 rule(s5, isCulprit(C,Att), [governmentLinked(P,C),identifiedIndividualInAttack(P,Att)]).
 
 
 rule(c, not(isCulprit(X,Att)), [isCulprit(Y,Att), not(X==Y)]).
-%%%%
-
 
 % preferences
 rule(p0, prefer(s2,s1), []).
---
+++ src/ex3.pl
@@ -0,0 +1,60 @@
+:- compile('gorgias-src-0.6d/lib/gorgias.pl').
+:- compile('gorgias-src-0.6d/ext/lpwnf.pl').
+
+% ex 2 - US bank hack
+rule(t1, culpritIsFrom(X, Att), [majorityIpOrigin(X, Att)]).
+rule(t2, not(culpritIsFrom(X, Att)), [spoofedIp(IP), ipGeoloc(G, IP), geolocInCountry(G, X), attackSourceIP(IP, Att)]).
+rule(t3, culpritIsFrom(X, Att), [firstLanguage(L, X), sysLanguage(L, Att)]).
+rule(t4, highLevelSkill(Att), [hijackCorporateClouds(Att)]).
+rule(t5, highLevelSkill(Att), [sophisticatedMalware(M), malwareUsedInAttack(M, Att)]).
+rule(t6(X), culpritIsFrom(X, Att), [infraRegisteredIn(X, Infra), infraUsed(Infra, Att)]).
+rule(t7(X), culpritIsFrom(X,Att), [languageInCode(L,Att),firstLanguage(L,X)]).
+rule(t8(X), isCulprit(X), [similar(M1,M2),malwareUsedInAttack(M1,Att),malwareUsedInAttack(M2,A2),target(T,A2),target(T,A1),isCulprit(X,A2)]).
+rule(t9(M), not(forBlackMarketUse(M)), [infectionMethod(usb,M)]).
+rule(t10(M), not(forBlackMarketUse(M)), [controlAndCommandEasilyFingerprinted(M)]).
+rule(t11, not(highLevelSkill(Att)), [forBlackMarketUse(M),malwareUsedInAttack(M,Att)]).
+
+rule(o1, requireHighResource(Att), [highLevelSkill(Att)]).
+rule(o2, hasCapability(_, Att), [not(requireHighResource(Att))]).
+rule(o3(C), hasCapability(C, Att), [requireHighResource(Att), hasResources(C)]).
+rule(o4(C), hasMotive(C, Att), [hasPoliticalMotive(C, T), target(T, Att)]).
+rule(o5(C,T), hasPoliticalMotive(C, T), [imposedSanctions(T, C)]).
+rule(o6(C), hasMotive(C, Att), [hasEconomicMotive(C, Industry), industry(Industry, T), target(T, Att)]).
+rule(o7, requireHighResource(Att), [highSecurity(T), target(T, Att)]).
+rule(o8, requireHighResource(Att), [highVolumeAttack(Att),longDurationAttack(Att)]).
+rule(o9(P,C), governmentLinked(P,C), [geolocatedInGovFacility(P,C)]).
+rule(o10(P,C), governmentLinked(P,C), [publicCommentsRelatedToGov(P,C)]).
+
+rule(s1, isCulprit(G,Att), [claimedResponsibility(G,Att)]).
+rule(s2, isCulprit(C,Att), [hasMotive(C,Att),hasCapability(C,Att)]).
+rule(s3, isCulprit(C,Att), [hasMotive(C,Att),culpritIsFrom(C,Att)]).
+rule(s4, not(isCulprit(C,Att)), [culpritIsFrom(C,Att),nothasCapability(C,Att)]).
+rule(s5, isCulprit(C,Att), [governmentLinked(P,C),identifiedIndividualInAttack(P,Att)]).
+rule(s6, not(isCulprit(C,Att)), [not(highLevelSkill(Att)),isCountry(C)]).
+
+rule(c, not(isCulprit(X,Att)), [isCulprit(Y,Att), not(X==Y)]).
+
+% preferences
+rule(p0, prefer(s2,s1), []).
+rule(p1, prefer(t2,t1), []).
+% ex2
+rule(p2, prefer(s4,s3), []).
+rule(p3, prefer(s5,s4), []).
+
+
+% evidences
+rule(f1, sophisticatedMalware(gauss), []).
+rule(f2, malwareUsedInAttack(gauss,attack), []).
+rule(f3, similar(gauss,stuxnet), []).
+rule(f4, malwareUsedInAttack(stuxnet,stuxnetattack), []).
+rule(f5, isCulprit(israel,stuxnetattack), []).
+rule(f6, isCulprit(us,stuxnetattack), []).
+rule(f7, target(middleEast,attack), []).
+rule(f8, target(israel,attack), []).
+rule(f9, target(lebanon,attack), []).
+rule(f10, target(palestine,attack), []).
+rule(f11, target(iran,stuxnetattack), []).
+rule(f12, infectionMethod(usb,gauss), []).
+rule(f13, controlAndCommandEasilyFingerprinted(gauss), []).
+rule(f14, hasPoliticalMotive(us,iran), []).
+rule(f15, hasPoliticalMotive(israel,iran), []).
