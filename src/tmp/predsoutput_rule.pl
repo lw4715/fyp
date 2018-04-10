ba5d5db5529f51d8e0f80f7dc2cf875037e6dd31
Linna Wang <linna.wang15@imperial.ac.uk>
2018-04-10 18:37:13 +0100

    clean

--- src/tmp/predsoutput_rule.pl
+++
@@ -1,8 +0,0 @@
-hasCapability(X,A,D0) :- prove([hasCapability(X,A)], D0).
-hasMotive(X,A,D1) :- prove([hasMotive(X,A)], D1).
-governmentLinked(P,X,D2) :- prove([governmentLinked(P,X)], D2).
-
-goal(A, M, X, D0, D1, D2) :- 
-  (hasCapability(X,A,D0), writeToFile(hasCapability(X,A), 0); \+ hasCapability(X,A,D0), write(neg(hasCapability(X,A)))), nl,
-  (hasMotive(X,A,D1), writeToFile(hasMotive(X,A), 1); \+ hasMotive(X,A,D1), write(neg(hasMotive(X,A)))), nl,
-  (governmentLinked(P,X,D2), writeToFile(governmentLinked(P,X), 2); \+ governmentLinked(P,X,D2), write(neg(governmentLinked(P,X)))), nl.
--- src/tmp/tmpgorgias.pl
+++
@@ -1,3 +0,0 @@
-rule(0, highLevelSkill(Att), [stolenValidSignedCertificates(Att)]).
-rule(1, specificTarget(Att), [specificConfigInMalware(M),malwareUsedInAttack(M,Att)]).
-rule(2, sophisticatedMalware(M), [usesZeroDayVulnerabilities(M)]).
