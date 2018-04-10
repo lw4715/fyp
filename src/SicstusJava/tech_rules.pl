4cf7dd28a2f00011d913397b71378174fbe59523
Linna Wang <linna.wang15@imperial.ac.uk>
2018-04-10 15:10:48 +0100

    not equals

--- src/SicstusJava/tech_rules.pl
+++ src/SicstusJava/tech_rules.pl
@@ -29,17 +29,22 @@ rule(infra, culpritIsFrom(X, Att), [infraRegisteredIn(X, Infra), infraUsed(Infra
 rule(bm, notForBlackMarketUse(M), [infectionMethod(usb,M),controlAndCommandEasilyFingerprinted(M)]). 
 
 %% rule(similarDefault, neg(similar(_M1, _M2)), []).
-same(X,X).
-rule(similar, similar(M1, M2), [similarCCServer(M1, M2), \+ same(M1,M2)]). % try same(A,A)
-rule(simCC, similarCCServer(M1, M2), [ccServer(S, M1), ccServer(S, M2)]).
-rule(simCC, similarCCServer(M1, M2), [ccServer(S1, M1), ccServer(S2, M2),
-  ccServerAddrType(S1,T),ccServerAddrType(S2,T), \+ same(S1,S2)]).
+rule(similar, similar(M1, M2), [similarCCServer(M1, M2), M1 \= M2]).
+rule(simCC1, similarCCServer(M1, M2), [ccServer(S, M1), ccServer(S, M2)]).
+%% rule(simCC2, similarCCServer(M1, M2), [ccServer(S1, M1), ccServer(S2, M2), \+ S1 = S2,
+  %% ccServerAddrType(S1,T),ccServerAddrType(S2,T)]).
+rule(simCC2, similarCCServer(M1, M2), [ccServer(S1, M1), ccServer(S2, M2), S1 \= S2,
+  domainRegisteredDetails(S1,_,A), domainRegisteredDetails(S2,_,A)]).
+rule(simCC3, similarCCServer(M1, M2), [ccServer(S1, M1), ccServer(S2, M2), S1 \= S2,
+  domainRegisteredDetails(S1,Name,_), domainRegisteredDetails(S2,Name,_)]).
+
 rule(ccServerAddrType, ccServerAddrType(Server, Type),
   [domainRegisteredDetails(Server,_,Addr),addressType(Addr,Type)]). 
 %TODO can link to googlemaps?
 rule(similar1, similar(M1, M2), [simlarCodeObfuscation(M1, M2)]).
 rule(similar2, similar(M1, M2), [sharedCode(M1, M2)]).
 rule(similar3, similar(M1, M2), [malwareModifiedFrom(M1, M2)]).
+rule(similar4, similar(M1, M2), [M1 \= M2, fileCharaMalware(C1, M1), fileCharaMalware(C2,M2), similarFileChara(C1, C2)]).
 
 rule(targetted, specificTarget(Att), [specificConfigInMalware(M),malwareUsedInAttack(M,Att)]).
 rule(zeroday, sophisticatedMalware(M), [usesZeroDayVulnerabilities(M)]).
@@ -48,7 +53,6 @@ rule(zeroday, sophisticatedMalware(M), [usesZeroDayVulnerabilities(M)]).
 %% rule(bg111, fileChara('@WanaDecryptor@exe','7bf2b57f2a205768755c07f238fb32cc','245760','2009-07-1323:19:35Z','Decryptor','EXE',wannacry_filechara4), []).
 %% 'Filename', 'MD5 Hash', 'Size (bytes)', 'Compile Time', 'Description', 'Filetype', malware 
 
-rule(similar4, similar(M1, M2), [fileCharaMalware(C1, M1), fileCharaMalware(C2,M2), similarFileChara(C1, C2)]).
 rule(similarFileChara1, similarFileChara(C1, C2), [fileChara(Filename,_,_,_,_,_,C1), fileChara(Filename,_,_,_,_,_,C2)]).
 rule(similarFileChara2, similarFileChara(C1, C2), [fileChara(_,MD5,_,_,_,_,C1), fileChara(_,MD5,_,_,_,_,C2)]).
 rule(similarFileChara3, similarFileChara(C1, C2), [fileChara(_,_,_,_,Desc,_,C1), fileChara(_,_,_,_,Desc,_,C2)]).
